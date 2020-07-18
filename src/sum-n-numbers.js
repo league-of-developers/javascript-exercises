c393d3c5f4ee624b6c0b8d7b4aec6b6b4671b6ee
thiagovieira461 <thiagovieira_686@hotmail.com>
2020-07-18 18:05:03 -0300

    WIP

---
+++ src/sum-n-numbers.js
@@ -0,0 +1,12 @@
+const arraySum = require("./sum-array-exercise")
+
+const solveExpressionOfNaturalNumbersUntilN = ({number}) => {
+
+    let sum = 1;
+    
+    for (let i = 0; i < number; i += 1) {
+        sum 
+    }
+
+    return sum;
+}
