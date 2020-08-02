3f73ca09cf4affcba9169332a1aba6ac584b1abc
thiagovieira461 <thiagovieira_686@hotmail.com>
2020-08-02 18:50:24 -0300

    Total coverage in test

--- test/calculate-n-negative-odd-numbers-and-positives-even-numbers.test.js
+++ test/calculate-n-negative-odd-numbers-and-positives-even-numbers.test.js
@@ -1,5 +1,7 @@
 const assert = require('assert');
 const { describe, it } = require('mocha');
+const { expect } = require('chai');
+
 const calculateNegativePositive = require('../src/calculate-n-negative-odd-numbers-and-positives-even-numbers');
 
 describe('Function that receives a number and calculates the following expression: 1 -2 +3 - 4 +... - n.', () => {
@@ -9,5 +11,10 @@ describe('Function that receives a number and calculates the following expressio
     const result = calculateNegativePositive({ number });
 
     assert(expectedValue, result);
+  });
+
+  it('Should throw error if number is negative', () => {
+    const number = -4;
+    expect(() => calculateNegativePositive({ number })).to.throw('number must be positive');
   });
 });
