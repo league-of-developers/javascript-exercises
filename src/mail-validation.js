f15488e82720a76d88bef99a8d71737232aab898
Juliano Lima <juliano.lima@nubank.com.br>
2020-08-02 15:28:25 -0300

    Fix the mail validation function

--- src/mail-validation.js
+++ src/mail-validation.js
@@ -1,6 +1,6 @@
 const mailValidation = ({ mail }) => {
   const regex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-  return re.test(String(mail).toLowerCase());
+  return regex.test(String(mail).toLowerCase());
 };
 
 module.exports = mailValidation;
